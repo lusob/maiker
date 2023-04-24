77fafa325f7aaa14f1cb0b3ee41a0a1149914fe0
Luis Sobrecueva Menendez <luis.sobrecueva@cabify.com>
2023-04-24 17:36:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+![image](./assets/maiker-logo.png)
+
+M**ai**ker is a VS Code extension that generates a project based on a user prompt using OpenAI API to generate the content.
+
+## 🎥 Demo April 24th 2023
+
+<video width="960" height="540" controls allowfullscreen>
+  <source src="./assets/maiker.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+
+## ✨ Features
+
+Maiker use an OpenAI model to generate a code project based on the user prompt. The files will be returned in separated files.
+
+## ⚙️ Requirements
+
+Maiker requires an OpenAI API key to use the OpenAI API. To get an API key, you can sign up for the OpenAI API beta program on the OpenAI website.
+
+## 🛠️ Installation
+
+To install the m**ai**ker extension, follow these steps:
+
+1. Open Visual Studio Code.
+
+2. Open the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the VS Code window.
+
+3. Search for `maiker` in the search bar.
+
+4. Click the `Install` button next to the `maiker` extension in the search results.
+
+5. Wait for the installation to complete.
+
+6. Reload Visual Studio Code.
+
+7. Use the `maiker.generateProject` command to generate a new project typing a description.
+
+Enjoy!
+
+## ⚙️ Extension Settings
+
+This extension contributes the following settings:
+
+* `maiker.openaiApi`: Your OpenAI API key. To obtain an API key, follow the instructions [here](https://beta.openai.com/signup/).
+* `maiker.promptContext`: The context string to add to the user input as to create the final prompt.
+
+## ⚠️ Limitations
+
+It's a prototype, not polished product so it may not perform well in complex business scenarios
+
+A very intensive use can lead to high expenses, monitor your account and set API key limits to avoid scares
+
+## 🐞 Known Issues
+
+Maiker may occasionally generate incorrect or irrelevant content based on the user's input. Additionally, maiker may encounter errors when calling the OpenAI API if the API key is invalid or if there are issues with the OpenAI API service.
+
+Due to the limits of the model's context size, the generated project cannot have many lines of code, although it is still useful to create personalized boilerplates of complex projects
+
+## 📝 Release Notes
+
+### 1.0.0
+
+Initial release of maiker.
+
+### 1.1.0
+
+Added support for generating custom filenames based on user input.
+
+### 1.2.0
+
+Added support for generating content for multiple files simultaneously.
+
+## 👨‍💻 Developers
+
+Maiker was created by [@lusob](https://twitter.com/lusob) for educational and demonstration purposes. Feel free to reach out on Twitter if you have any questions or feedback!
+
+---
---
+++ extension.js
@@ -0,0 +1,98 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs');
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = vscode.workspace.getConfiguration('maiker');
+const openaiApiKey = configuration.get('openaiApi');
+const promptContext = configuration.get('promptContext');
+
+const openai_configuration = new Configuration({
+	apiKey: openaiApiKey,
+});
+
+const openai = new OpenAIApi(openai_configuration);
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "maiker" is now active!');
+
+
+	// Define command to trigger code generation
+	const generateProjectCommand = vscode.commands.registerCommand('maiker.generateProject', async () => {
+		try {
+			// Get user prompt
+			const userInput = await vscode.window.showInputBox({ prompt: 'Describe here the project you want to create' });
+			console.log("User prompt: " + userInput)
+			// Show progress indicator
+			await vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: 'Generating project... (it may take a few minutes)',
+				cancellable: false
+			}, async () => {
+				try {
+					const completion = await openai.createChatCompletion({
+						model: "gpt-3.5-turbo",
+						messages: [
+							{ "role": "system", "content": `${promptContext}` },
+							{ "role": "user", "content": userInput }],
+						temperature: 0
+					});
+					console.log(completion.data.choices);
+					var msg_content = completion.data.choices[0].message.content
+					
+					// Preserve newlines, etc. - use valid JSON
+					msg_content = msg_content.replace(/`/g, "'").replace(/\\s/g, "\\s");
+
+					console.log("message content: " + msg_content)
+
+					const generatedCode = JSON.parse(msg_content);
+					console.log(generatedCode)
+					// Create directories for each file
+					for (const [filename, contents] of Object.entries(generatedCode)) {
+						//const directory = filename.substring(0, filename.lastIndexOf('/'));
+						console.log("filename: " + filename)
+						fs.writeFileSync("/home/lusob/projects/maiker-generated-project/" + filename, contents);
+					}
+
+					// Show success message to user
+					vscode.window.showInformationMessage('Code generation successful!');
+				} catch (error) {
+					if (error.response) {
+						console.log(error.response.status);
+						console.log(error.response.data);
+					} else {
+						console.log(error.message);
+					}
+					vscode.window.showErrorMessage('Code generation failed: ' + error.message);
+				}
+			});
+		} catch (error) {
+			// Show error message to user
+			vscode.window.showErrorMessage('Code generation failed: ' + error.message);
+		}
+	});
+
+	context.subscriptions.push(generateProjectCommand);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
+
+
